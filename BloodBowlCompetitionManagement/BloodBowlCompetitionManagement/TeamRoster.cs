02cc2412d8977280d4a1b530e736be7f1a240489
Luca Morleschi <eloreden@gmail.com>
2020-01-20 18:49:57 +0100

    #Sincronizzazione

---
+++ BloodBowlCompetitionManagement/BloodBowlCompetitionManagement/TeamRoster.cs
@@ -0,0 +1,11 @@
+﻿namespace VetryaBloodBowl.DAL
+{
+    internal class TeamRoster
+    {
+        public int ID { get; set; }
+        public object Race { get; set; }
+        public int RerollQuantity { get; set; }
+        public int RerollCost { get; set; }
+        public string SpecialRules { get; set; }
+    }
+}
